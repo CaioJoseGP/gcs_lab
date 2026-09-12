7ed885801ef6ffdc69a069bae5d95bf2cabaded9
caio <caio.jose@estudante.ifgoiano.edu.br>
2026-09-12 16:41:44 -0300

    feat: criacao continuous integration na esteira do github actions

---
+++ test/java/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import org.junit.juniper.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculadoraTest {
+    @Test
+    void testOperacoes() {
+        Calculadora calc = new Calculadora();
+
+        assertEquals(36, calc.somar(16, 20));
+        assertEquals(0, calc.subtrair(10, 10));
+    }
+}
