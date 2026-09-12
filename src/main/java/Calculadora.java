0e334f34eeed3039338881f6a8f4600fed00673f
caio <caio.jose@estudante.ifgoiano.edu.br>
2026-09-12 16:59:17 -0300

    docs: documentacao de metodos javadocs

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,5 +1,20 @@
+/**
+ * Classe responsável por operações aritméticas básicas.
+ */
 public class Calculadora {
+    /**
+     * Realiza a soma de dois números racionais (double).
+     * @param a Primeiro operando.
+     * @param b Segundo operando.
+     * @return O resultado da soma entre a e b.
+     */
     double somar(double a, double b) { return a + b; }
 
+    /**
+     * Realiza a subtração de dois números racionais (double).
+     * @param a Primeiro operando.
+     * @param b Segundo operando.
+     * @return O resultado da subtração entre a e b.
+     */
     double subtrair(double a, double b) { return a - b; }
 }
